788825d658478a15763cef1969ab464d84773917
PinHoong <e0725207@u.nus.edu>
2023-01-14 20:01:03 +0800

    front end changes

--- API/main.py
+++ API/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, request
-import flask.scaffold
-flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import requests
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,6 +22,22 @@ QrInformation_get_args = reqparse.RequestParser()
 QrInformation_get_args.add_argument("bankAccountNumber", type=str,help="Merchant Bank Account Number is required", required=True)
 QrInformation_get_args.add_argument("transactionAmount", type=str,help="Transaction Amount is required", required=True)
 
+Loan_get_args = reqparse.RequestParser()
+Loan_get_args.add_argument("transactionAmount", type=float,help="Transaction Amount is required", required=True)
+Loan_get_args.add_argument("merchantAccount", type=str,help="merchantAccount is required", required=True)
+
+Loan_put_args = reqparse.RequestParser()
+Loan_put_args.add_argument("transactionAmount", type=float,help="Transaction Amount is required", required=True)
+Loan_put_args.add_argument("merchantAccount", type=str,help="Merchant Account is required", required=True)
+
+PANDA_BANK_ACCOUNT_NUMBER= "888-8888-88"
+
+accounts = {"652-3342-22": {"Account Holder": "IKEA", "Balance": 2002.87},
+            "923-1234-66":{"Account Holder": "Singapore General Hospital", "Balance": 6008.73},
+            "123-4567-89":{"Account Holder": "Courts", "Balance": 78920.43},
+            PANDA_BANK_ACCOUNT_NUMBER: {"Account Holder": "Panda Bank", "Balance": 9999999}
+            }
+
 class Exchange(Resource):
     def post(self):
         args = exchange_get_args.parse_args()
@@ -32,7 +45,7 @@ class Exchange(Resource):
         response = requests.get(url).json()
         rate = response['rates']['ETH']
         return {'ETH': args['usd'] / rate}, 201
-    
+   
 
 class Collateral(Resource):
 
@@ -57,23 +70,105 @@ class Collateral(Resource):
             "Authorization": "c25da947-4699-47cb-b783-43d795249e67"
         }
         # args = collateral_get_args.parse_args()
-        url = "https://api.nftport.xyz/v0/accounts/" + walletAddress + "?chain=ethereum&page_size=50&include=metadata"
+        url = "https://api.nftport.xyz/v0/accounts/" + walletAddress + "?chain=goerli&page_size=50&include=metadata"
         response = requests.get(url, headers=headers)
         response = response.json()
-        return response, 201
+        nftList = []
+        for nft in response['nfts']:
+            nftList.append({"token_id": nft['token_id'], "contract_address": nft['contract_address']})
+            headers = {"accept": "application/json"}
+        for nft in nftList:
+            url = f"https://eth-goerli.g.alchemy.com/nft/v2/TaUU5yuSMWTtAY2xs4ymwzZoBpSN_Pe4/getNFTMetadata?contractAddress={nft['contract_address']}&tokenId={nft['token_id']}&refreshCache=false"
+            response = requests.get(url, headers=headers)       
+            response = response.json()
+            nft["file_url"] = response["media"][0]["raw"]
+            nft["name"] = response["title"]
+        return {'nfts':nftList},201
+
+def approveLoan(transactionAmount, merchantAccount):
+    if merchantAccount not in accounts:
+        raise ValueError("Invalid Merchant Account Number")
+    return transactionAmount <= accounts[PANDA_BANK_ACCOUNT_NUMBER]["Balance"]
+
+def loanTransfer(transactionAmount, merchantAccount):
+    if merchantAccount not in accounts:
+        raise ValueError("Invalid Merchant Account Number")
+    accounts[PANDA_BANK_ACCOUNT_NUMBER]["Balance"] -= transactionAmount
+    accounts[merchantAccount]["Balance"] += transactionAmount
 
 
-class QrInformation(Resource):
+
+class Loan(Resource):
+    def post(self):
+        args = Loan_get_args.parse_args()
+        transactionAmount = args['transactionAmount']
+        merchantAccount = args['merchantAccount']
+        try:
+            loanApproved = approveLoan(transactionAmount, merchantAccount)
+            response = {"Approved": loanApproved}
+            if loanApproved:
+                return response,201
+            return response, 422
+        except:
+            return {"Message": "Invalid Account Number"},404
+
+    def put(self):
+        args = Loan_put_args.parse_args()
+        transactionAmount = args['transactionAmount']
+        merchantAccount = args['merchantAccount']
+        try:
+            if not approveLoan(transactionAmount, merchantAccount):
+                return {"Message": "Loan was not approved"},422
+            print("before:", accounts[PANDA_BANK_ACCOUNT_NUMBER])
+            print("before:",accounts[merchantAccount])
+            loanTransfer(transactionAmount,merchantAccount)
+            print("after", accounts[PANDA_BANK_ACCOUNT_NUMBER])
+            print("after",accounts[merchantAccount])
+            return {"Message": "Loan Amount has been transferred"}, 201
+        except:
+            return {"Message": "Invalid Account Number"},404
+
+
+class Account(Resource):
+    def get(self,accountNumber):
+        if accountNumber not in accounts:
+            return {"Message":"Invalid Account Number"},404
+        return {"accountNumber": accountNumber} | accounts[accountNumber], 201
+
+
+
+def generateMerchantQR(merchantAccountNumber, transactionAmount, merchantName):
+    if merchantAccountNumber not in accounts:
+        raise ValueError("Invalid Merchant Account Number")
+
+    inputData = merchantAccountNumber + " " + transactionAmount
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+    qr.add_data(inputData)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    merchantQR = merchantName + "QR.png"
+    img.save(merchantQR)
+
+
+class QR(Resource):
     def get(self):
         args = QrInformation_get_args.parse_args()
         print(request.form)
+
+    def put(self):
+        pass
 
 
 
 
 api.add_resource(Collateral, "/collateral/<string:walletAddress>")
 api.add_resource(Exchange, "/exchange")
-api.add_resource(QrInformation,"/transact")
+api.add_resource(Loan, "/loan")
+api.add_resource(Account, "/account/<string:accountNumber>")
+#api.add_resource(QrInformation,"/transact")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ pages/nftLibrary.js
@@ -0,0 +1,446 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router"
+import { useEffect, useState, React } from 'react'
+import {ethers} from "ethers"
+import { Navbar, Button, Link, Text, Card, Spacer, Radio, useTheme, Col, Row, Grid, Container} from '@nextui-org/react';
+import { AcmeLogo } from '../components/AcmeLogo'
+export default function Home() {
+
+  var ContractAddress = "0xE1Fb9d4188cBd85339Ca4260FA36f3db484B433E";
+  var ContractABI = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "loanAmount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "loanCompleteTime",
+          "type": "uint256"
+        }
+      ],
+      "name": "LoanCreated",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        }
+      ],
+      "name": "LoanDrawn",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "lender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "repayer",
+          "type": "address"
+        }
+      ],
+      "name": "LoanRepayed",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "lender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "caller",
+          "type": "address"
+        }
+      ],
+      "name": "LoanSeized",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "lender",
+          "type": "address"
+        }
+      ],
+      "name": "LoanUnderwritten",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_tokenid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_loanAmount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_loanCompleteTime",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_interestRate",
+          "type": "uint256"
+        }
+      ],
+      "name": "createLoan",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_loanId",
+          "type": "uint256"
+        }
+      ],
+      "name": "drawLoan",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "nftLoans",
+      "outputs": [
+        {
+          "internalType": "address",
+"name": "tokenAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "tokenOwner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "lender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "tokenId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "loanAmount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "loanCompleteTime",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "loanAmtWithInterest",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "numLoans",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_loanId",
+          "type": "uint256"
+        }
+      ],
+      "name": "repayLoan",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_loanId",
+          "type": "uint256"
+        }
+      ],
+      "name": "seizeNFT",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_loanId",
+          "type": "uint256"
+        }
+      ],
+      "name": "underwriteLoan",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+  ];
+
+  async function setSafeApproval(NFTContract, NFTToken, Amount, UnixTimeStamp, InterestRate) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    const NFTABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getmintCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxMintAmountPerTx","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"mintCost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"mintCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"uriPrefix","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"uriSuffix","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"walletOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+    const tx = new ethers.Contract(NFTContract, NFTABI, signer)
+    var response = await tx.setApprovalForAll(ContractAddress, true);
+    var transactionHash = response["hash"];
+    const isTransactionMined = async(transactionHash) => {
+      var txReceipt = await provider.getTransactionReceipt(transactionHash);
+      while (txReceipt == null) {
+        txReceipt = await provider.getTransactionReceipt(transactionHash);
+        console.log('I am waiting!');
+      }
+      initialise(NFTContract, NFTToken, Amount, UnixTimeStamp, InterestRate);
+    }
+
+    await isTransactionMined(transactionHash);
+  }
+
+  async function initialise(NFTContract, NFTToken, Amount, UnixTimeStamp, InterestRate) {
+
+    const response = await fetch("http://127.0.0.1:5000/loan", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"transactionAmount": Amount, "merchantAccount": "652-3342-22"})
+    })
+    if (response.status == 201) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send('eth_requestAccounts', []);
+      const signer = provider.getSigner();
+  
+      const Contract = new ethers.Contract(ContractAddress, ContractABI, signer);
+  
+      const loanid = await Contract.createLoan(NFTContract, NFTToken, Amount, UnixTimeStamp, InterestRate);
+
+      fetch("http://127.0.0.1:5000/loan", {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"transactionAmount": Amount, "merchantAccount": "652-3342-22"})
+      })
+      
+    } else {
+      console.log("Panda Bank is a brokie!")
+    }
+  }
+
+  const router = useRouter()
+  const {
+    query: {userWallet}, 
+  } = router 
+
+  
+  
+
+  const props = {userWallet} 
+  console.log(props.userWallet)
+  const axios = require('axios');
+  const [data, setData] = useState('');
+  const callAPI = async () => {
+    const data = await fetch('http://127.0.0.1:5000/collateral/' + props.userWallet).then(res => res.json())
+    setData(data);
+  }
+
+  useEffect(() =>{
+    callAPI()
+  }, [])
+  console.log(data['nfts'])
+  var date = new Date()
+  date.setDate(date.getDate() + 30)
+
+  date = String(Math.round(date/1000))
+    const [variant, setVariant] = useState("default");
+  const [activeColor, setActiveColor] = useState("primary");
+
+  const {isDark} = useTheme();
+
+  const variants = [
+    "default",
+    "highlight",
+    "highlight-solid",
+    "underline",
+    "highlight-rounded",
+    "highlight-solid-rounded",
+    "underline-rounded",
+  ];
+
+  const colors = ["primary", "secondary", "success", "warning", "error"];
+  
+  return (
+    <div>
+
+      <Navbar isBordered variant="sticky">
+        <Navbar.Brand>
+            <AcmeLogo />
+            <Text b color="inherit" hideIn="l" h3>
+              FlexiPay
+            </Text>
+        </Navbar.Brand>
+        <Navbar.Content>
+          <Navbar.Link href="/">Home</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="/signin">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="/signin">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+
+      <Spacer y={1} />
+      <Container gap="5">
+        <Row>
+      {data['nfts']?.map(user => (
+        <Col gap="5">
+                    {/* <h1>{user.name}</h1>
+                    <img src= {"https://ipfs.io/ipfs/" + user.file_url.slice(7)} alt='me' width='200' height='200'/>
+                    <button className='button-37' onClick={()=>{
+                      setSafeApproval(user.contract_address, user.token_id, "20", date, "105")
+                    }}>Select</button> */}
+                    <Card css={{ mw: "230px" }}>
+                      <Card.Header> 
+                      <Text b>{user.name}</Text>
+                      </Card.Header>
+                      <Card.Body css={{ py: "$10" }}>
+                      <img src= {"https://ipfs.io/ipfs/" + user.file_url.slice(7)} alt='me' width='200' height='200'/>
+                      </Card.Body>
+                      <Card.Divider />
+                      <Card.Footer>
+                      <Row justify="flex-end">
+                      <Button size="sm" onClick={()=>{
+                      setSafeApproval(user.contract_address, user.token_id, "20", date, "105")
+                    }}>Agree</Button>
+                      </Row>
+                      </Card.Footer>
+                    </Card>
+                    
+                </Col>
+            ))}
+            </Row>
+            </Container>
+        </div>
+  )
+}
